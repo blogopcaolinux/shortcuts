7f86694bb0051d6390aeca732b597b9704a3cbf2
sandrodecastro <sandrodecastro.cp@gmail.com>
2017-04-24 12:31:15 -0300

    Criado bkpconfs.sh
    Script para fazer backup dos arquivos de configuração dos sistemas Linux

---
+++ bkpconfs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+## Script elaborado pelo Morvan ##
+arq=$HOSTNAME.confs.zip
+March=`echo $MACHTYPE | cut -c8-13`
+echo "Salvando Dados no Arquivo $arq com Arquitetura $March"
+# localiza e salva os .rules, .conf e os .ini
+locate .rules | grep /etc/ | zip $arq -@ -9
+locate .conf | grep /etc/ | zip $arq -@ -9
+locate .ini | grep /etc/ | zip $arq -@ -9
+zip $arq -r  -9 /etc/{fstab,default/grub,hostname}
+if [ $March != "redhat" ] ; then # Salva Dados de Debian Like
+   zip $arq -r  -9 /etc/apt/sources.list 
+else # salva dados a la RH
+   zip $arq -r  -9 /etc/yum.repos.d/*
+fi
+exit
