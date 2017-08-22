5fa79385e5d0938dc4248f88a978cb80da02ca3b
Sandro de Castro <sandrodecastro.cp@gmail.com>
2017-08-21 22:58:27 -0300

    Update bkpconfs.sh

--- bkpconfs.sh
+++ bkpconfs.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-## Script elaborado pelo Morvan ##
-arq=$HOSTNAME.confs.zip
+## Script elaborado por Morvan ##
 March=`echo $MACHTYPE | cut -c8-13`
-echo "Salvando Dados no Arquivo $arq com Arquitetura $March"
-# localiza e salva os .rules, .conf e os .ini
-locate .rules | grep /etc/ | zip $arq -@ -9
-locate .conf | grep /etc/ | zip $arq -@ -9
-locate .ini | grep /etc/ | zip $arq -@ -9
-zip $arq -r  -9 /etc/{fstab,default/grub,hostname}
+Mdia=`date +%Y%m%d`
+arq=$HOSTNAME.confs.$Mdia.zip
+com1="locate"
+com2="zip"
+para="-r -9 -@"
+echo -e "\a\033[01;34mSalvando Dados no Arquivo\033[01;31m $arq \a\033[01;00mcom Arquitetura \033[01;31m$March\033[01;00m"
+# localiza e salva os .conf e os .ini, etc
+$com1 .conf | grep /etc/ | $com2 $arq $para >> $arq.log
+$com1 .ini | grep /etc/ | $com2 $arq $para >> $arq.log
+$com1 .rules | grep /etc/ | $com2 $arq $para >> $arq.log
+$com1 .sh | grep ".sh$" | grep /home/morvan | $com2 $para $arq >> $arq.log
+$com1 /etc/{fstab,default/grub,hostname} | $com2 $para $arq >> $arq.log
 if [ $March != "redhat" ] ; then # Salva Dados de Debian Like
-   zip $arq -r  -9 /etc/apt/sources.list 
+ $com1 /etc/apt/sources.list | $com2 $para $arq >> $arq.log
 else # salva dados a la RH
-   zip $arq -r  -9 /etc/yum.repos.d/*
+ $com1 /etc/yum.repos.d/* | $com2 $para $arq >> $arq.log
 fi
 exit
